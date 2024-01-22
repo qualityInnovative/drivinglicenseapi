30f8040a94fa2e0d00999cf02c51daea914ec0a4
ubaidh <obaidh@live.com>
2024-01-22 15:38:22 +0530

    update

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(function (req, res, next) {
     );
 
     // Additional security headers
+    res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
     res.setHeader("X-Frame-Options", "DENY");
     res.setHeader("X-Content-Type-Options", "nosniff");
     res.setHeader("X-XSS-Protection", "1; mode=block");
